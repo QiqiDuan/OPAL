bf7d0fbffda1eb0b410b2c21b99ed80521f13a8c
QiqiDuan <2332917051@qq.com>
2017-08-19 10:13:03 +0800

    v00.00.000
    first commit

---
+++ matlab/LSGO2013-2015/RS.m
@@ -0,0 +1,50 @@
+function [opt_fp, opt_fv, run_time, num_fe] = ...
+    RS(fhd, fun_ind, fun_dim, slb, sub, pop_size, num_fe, max_fe, rsip)
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% %
+% Random Search [i.e. RS] based on the parallel population, where all
+%   individuals are independent each other during iterations.
+%
+% -----------------
+% || INPUT  || <---
+% -----------------
+%   fhd        <--- str2func, function handler
+%   fun_ind    <--- matrix(1, 1), function index
+%   fun_dim    <--- matrix(1, 1), function dimension
+%   slb        <--- matrix(pop_size, fun_dim), search lower bound
+%   sub        <--- matrix(pop_size, fun_dim), search upper bound
+%   pop_size   <--- matrix(1, 1), population size
+%   num_fe     <--- matrix(1, 1), initial number of function evaluation
+%   max_fe     <--- matrix(1, 1), maximum of function evaluation
+%   rsip       <--- matrix(1, 1), rand seed for initializing the population
+%
+% -----------------
+% || OUTPUT || --->
+% -----------------
+%   opt_fp     ---> matrix(1, fun_dim), optimal function position
+%   opt_fv     ---> matrix(1, 1), optimal function value
+%   run_time   ---> matrix(1, 1), run time
+%   num_fe     ---> matrix(1, 1), final number of function evaluation
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% %
+    % initialize experimental parameters
+    run_time_start = tic;
+    RandStream.setGlobalStream(RandStream('mt19937ar', 'Seed', 'shuffle'));
+    
+    % initialize algorithmic parameters
+    fp = slb + (sub - slb) .* rand(RandStream('mt19937ar', 'Seed', rsip), fun_dim, pop_size)';
+    fv = feval(fhd, fp, fun_ind);
+    num_fe = num_fe + pop_size;
+    opt_fv = min(fv);
+    
+    while num_fe < max_fe
+        fp = slb + (sub - slb) .* rand(pop_size, fun_dim);
+        fv = feval(fhd, fp, fun_ind);
+        num_fe = num_fe + pop_size;
+        
+        if min(fv) < opt_fv
+            [opt_fv, min_ind] = min(fv);
+            opt_fp = fp(min_ind, :);
+        end
+    end
+    
+    run_time = toc(run_time_start);
+end
