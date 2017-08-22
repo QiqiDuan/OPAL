91590b6334950e9cbb50729bfc9333cd49a1df26
QiqiDuan <2332917051@qq.com>
2017-08-22 10:04:42 +0800

    v00.00.001
    correct typos

--- matlab/LSGO2013-2015/comp_mat/save_CCFR_IDG2_data.m
+++ matlab/LSGO2013-2015/comp_mat/save_CCFR_IDG2_data.m
@@ -1,3 +1,3 @@
 CCFR_IDG2 = load_CCFR_IDG2_data();
-CCFR_IDG2 = CCFR_IDG2(:, 1: 2); % 1 : 2 ---> Mean, Std
+CCFR_IDG2 = CCFR_IDG2(:, 1 : 2); % 1 : 2 ---> Mean, Std
 save('CCFR_IDG2.mat', 'CCFR_IDG2', '-v7.3');
